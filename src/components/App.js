6aca75a6d0427e54b8f030ec0a5b02bf926bfb36
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2020-05-25 00:58:20 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
-import './App.css';
+// jshint esversion : 6
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        App
-      </div>
-    );
-  }
-}
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import "./App.css";
+
+import ColorPanel from "./ColorPanel/ColorPanel";
+import SidePanel from "./SidePanel/SidePanel";
+import Messages from "./Messages/Messages";
+import MetaPanel from "./MetaPanel/MetaPanel";
+
+const App = () => (
+  <Grid columns="equal" className="app" style={{ background: "#eee" }}>
+    <ColorPanel />
+    <SidePanel />
+
+    <Grid.Column style={{ marginLeft: 320 }}>
+      <Messages />
+    </Grid.Column>
+
+    <Grid.Column width={4}>
+      <MetaPanel />
+    </Grid.Column>
+  </Grid>
+);
 
 export default App;
