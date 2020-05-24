d45f3233f6a9252566a5d5d8ce0c3d8de312b048
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2020-05-24 14:19:04 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// jshint esversion :6
+
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
@@ -15,10 +17,20 @@ import {
   withRouter
 } from "react-router-dom";
 
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+import { setUser } from "./actions";
+
+const store = createStore(rootReducer, composeWithDevTools());
+
 class Root extends React.Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
+        // console.log(user);
+        this.props.setUser(user);
         this.props.history.push("/");
       }
     });
@@ -35,12 +47,19 @@ class Root extends React.Component {
   }
 }
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(
+  connect(
+    null,
+    { setUser }
+  )(Root)
+);
 
 ReactDOM.render(
-  <Router>
-    <RootWithAuth />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <RootWithAuth />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 registerServiceWorker();
