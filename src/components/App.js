b2193bd90ac4b6413bdd68760686a74c7e77b282
Shivansh Dwivedi <52450815+shivanshdwivedi@users.noreply.github.com>
2020-05-26 16:53:35 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
-// jshint esversion : 6
+// jshint esversion:6
 
 import React from "react";
 import { Grid } from "semantic-ui-react";
 import "./App.css";
+import { connect } from "react-redux";
 
 import ColorPanel from "./ColorPanel/ColorPanel";
 import SidePanel from "./SidePanel/SidePanel";
 import Messages from "./Messages/Messages";
 import MetaPanel from "./MetaPanel/MetaPanel";
 
-const App = () => (
+const App = ({ currentUser }) => (
   <Grid columns="equal" className="app" style={{ background: "#eee" }}>
     <ColorPanel />
-    <SidePanel />
+    <SidePanel currentUser={currentUser} />
 
     <Grid.Column style={{ marginLeft: 320 }}>
       <Messages />
@@ -24,4 +25,8 @@ const App = () => (
   </Grid>
 );
 
-export default App;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(App);
